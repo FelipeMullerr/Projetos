1466af4c663da17fb8548439342a4294f2a90d30
Felipe Müller <felipemuller@MacBook-Pro-de-Felipe.local>
2025-04-05 19:39:15 -0300

    Alterando Arquivos

---
+++ banco.h
@@ -0,0 +1,21 @@
+#ifndef BANCO_H
+#define BANCO_H
+
+#define MAX_REGISTROS 100
+#define TAM_NOME 50
+#define PIPE_NOME "meu_pipe"
+
+typedef struct {
+    int id;
+    char nome[TAM_NOME];
+} Registro;
+
+extern Registro registros[MAX_REGISTROS];
+
+void inicializar_banco();
+int inserir_registro(int id, const char* nome);
+int deletar_registro(int id);
+int selecionar_registro(int id);
+void imprimir_banco();
+
+#endif
